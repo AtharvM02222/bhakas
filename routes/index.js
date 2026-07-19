eeb12dd42289c43788d5d046e2f32844c1421add
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-07-19 14:01:29 +0530

    feat: nav

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bhakas' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET shop page. */
+router.get('/shop', function(req, res, next) {
+  res.render('shop');
+});
+
 export default router;
