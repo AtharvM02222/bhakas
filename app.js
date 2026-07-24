1e07e14d7f1862e836e3835aa6a32463c550de17
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-07-24 15:50:24 +0530

    refactor: security

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { fileURLToPath } from 'url';
 import expressLayouts from 'express-ejs-layouts';
-import { firebaseConfig } from './public/javascripts/db.js';
+import { firebaseConfig } from './config/db.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
